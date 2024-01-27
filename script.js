c567be774118eef4d588ca4f4ba7279ae530f0e4
Chirag sadhwani <138006300+chiragop1234@users.noreply.github.com>
2024-01-27 13:39:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-const terminalContainer = document.getElementById('terminal');
-const input = document.getElementById('input');
-const term = new Terminal();
-
-term.open(terminalContainer);
-
-input.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        const command = input.value;
-        input.value = '';
-
-        term.write(`\r\n$ ${command}\r\nOutput: This is a simulated result.\r\n`);
-        term.write('$ ');
-    }
+$(function() {
+  $('.prompt').html('[' + codehelper_ip["IP"] + '@HTML5] # ');
+  var term = new Terminal('#input-line .cmdline', '#container output');
+  term.initFS();
 });
-
-term.write('$ ');
-
-term.focus();
